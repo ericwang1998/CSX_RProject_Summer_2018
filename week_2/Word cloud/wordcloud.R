75a44e07c30cb952f3f913c08b0c58b457841a71
ericwang1998 <40811893+ericwang1998@users.noreply.github.com>
2018-07-12 15:07:12 +0800

    Updates

--- week_2/Word cloud/wordcloud.R
+++ week_2/Word cloud/wordcloud.R
@@ -1,9 +1,21 @@
 #This worker initializes jiebaR workers.. what are they?
 library(jiebaR)
+library(wordcloud)
+library(magrittr)
 wk <- worker()
 
 #import text
 text <- readLines("Mayunspeech1.txt", encoding = "UTF-8")
+
+#place all the characters we don't want into a vector
+stopwords <- c("我", "的", "在", "和", "都", "也", "不", "就", "上", "为", "我们", "这", "从","是", "会", "个","有","像","把","其实")
+#make them into one whole string separated by '|'
+stopwords.pattern <- paste0(stopwords, sep = "|", collapse = "") %>%
+  substr(1, nchar(.) -1)
+# the one above takes them out. 
+
+#Pull the trigger. So if the text any of the phrases in the string(since they have the "or" sign), they will be removed.
+text <- gsub(stopwords.pattern, " ", text)
 
 #breaks apart the words.
 speech <- wk[text]
