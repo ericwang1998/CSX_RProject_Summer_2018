bea6466d514d62d3102f8e45491d747b97da2e63
ericwang1998 <40811893+ericwang1998@users.noreply.github.com>
2018-07-13 12:35:15 +0800

    Finally problem solved

--- week_2/Word cloud/wordcloud.R
+++ week_2/Word cloud/wordcloud.R
@@ -2,13 +2,16 @@
 library(jiebaR)
 library(wordcloud2)
 library(magrittr)
+Sys.setlocale("LC_ALL", locale = "Chinese")
 wk <- worker()
 
 #import text
 text <- readLines("Mayunspeech1.txt", encoding = "UTF-8")
 
 #place all the characters we don't want into a vector
-stopwords <- c("我","的","在","和","都","也","不","就","上","为","我们","你们","他们","这","从","是","会","个","有","像","把","其实","了", "谁","所","但")
+stopwords <- c("我","的","在","和","都","也","不","就","上","为","我们","你们","他们","这","从","是",
+               "会","个","有","像","把","其实","了", "谁","所","但", "更", "很","什么", "一","因", "让",
+               "好", "每", "多", "得", "当", "将","座", "对", "没", "你","到")
 #make them into one whole string separated by '|'
 
 stopwords.pattern <- paste0(stopwords, sep = "|", collapse = "") %>%
@@ -18,6 +21,7 @@ stopwords.pattern <- paste0(stopwords, sep = "|", collapse = "") %>%
 #Pull the trigger. So if the text any of the phrases in the string(since they have the "or" sign), they will be removed.
 text <- gsub(stopwords.pattern, " ", text)
 
+
 #breaks apart the words.
 speech <- wk[text]
 
@@ -26,6 +30,7 @@ speech <- as.data.frame(table(speech))
 
 #Ordering the phrases in terms of frequencies in decreasing order
 freq <- speech[order(speech$Freq, decreasing = T),]
+str(freq)
 
 #Drawing the wordcloud
 wordcloud2(freq, size = 1.5, color = "random-light", backgroundColor = "grey")
