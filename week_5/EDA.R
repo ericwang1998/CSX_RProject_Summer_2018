7e0b70b48a7f773fea2d7a338d4afc4e2427ad72
ericwang1998 <40811893+ericwang1998@users.noreply.github.com>
2018-08-03 14:03:46 +0800

    new data in

--- week_5/EDA.R
+++ week_5/EDA.R
@@ -1,9 +1,12 @@
 library(dplyr)
 library(ggplot2)
+library(Amelia)
 
 kiva <- readxl::read_xlsx("~/GitHub/CSX_RProject_Summer_2018/week_5/kiva/kiva_loans.xlsx")
 str(kiva)
 kiva$repayment_interval["male, male, female, male, female, female, male, female, male, female, male, female, male, female"]
 kiva<- kiva %>%
-  filter(repayment_interval == "male, male, female, male, female, female, male, female, male, female, male, female, male, female") %>%
-  sub
+  filter(repayment_interval == "male, male, female, male, female, female, male, female, male, female, male, female, male, female")
+
+missmap(kiva, col = c("grey", "black"), legend = FALSE, main = "Missing values map")
+
