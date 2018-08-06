4f9ef7652ed280e71ff173614e216d629517f307
ericwang1998 <40811893+ericwang1998@users.noreply.github.com>
2018-08-06 16:34:06 +0800

    Breakthroughs

--- week_5/Shiny_map/app.R
+++ week_5/Shiny_map/app.R
@@ -1,70 +1,76 @@
-
 library(shiny)
 library(leaflet)
 library(RColorBrewer)
+library(readxl)
+library(magrittr)
+
+kiva_mpi <- read_xlsx("~/GitHub/CSX_RProject_Summer_2018/week_5/data/kiva_mpi_region_locations.xlsx")
+kiva_mpi<- kiva_mpi[complete.cases(kiva_mpi), ]
+
+
+labels <- sprintf(
+  "<strong>%s</strong><br/>MPI: %g",
+  kiva_mpi$LocationName, kiva_mpi$MPI
+) %>% lapply(htmltools::HTML)
+
 
 ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", width = "100%", height = "100%"),
   absolutePanel(top = 10, right = 10,
-                sliderInput("range", "Magnitudes", min(quakes$mag), max(quakes$mag),
-                            value = range(quakes$mag), step = 0.1
+    sliderInput("range", "MPI", min(kiva_mpi$MPI), max(kiva_mpi$MPI),
+                value = range(kiva_mpi$MPI), step = 0.05
                 ),
-                selectInput("colors", "Color Scheme",
-                            rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
-                ),
-                checkboxInput("legend", "Show legend", TRUE)
+    selectInput("colors", "Color Scheme",
+                rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
+    ),
+    checkboxInput("legend", "Show legend", TRUE)
+    )
   )
-)
-server <- function(input, output, session) {
-  
-  # Reactive expression for the data subsetted to what the user selected
+
+server <- function(input, output, session){
   filteredData <- reactive({
-    quakes[quakes$mag >= input$range[1] & quakes$mag <= input$range[2],]
+    kiva_mpi[kiva_mpi$MPI >= input$range[1] & kiva_mpi$MPI <= input$range[2],]
   })
-  
-  # This reactive expression represents the palette function,
-  # which changes as the user makes selections in UI.
-  colorpal <- reactive({
-    colorNumeric(input$colors, quakes$mag)
+
+colorpal <- reactive({
+  colorNumeric(input$colors, kiva_mpi$MPI)
+})
+
+output$map <- renderLeaflet({
+  leaflet(kiva_mpi) %>% 
+    addTiles() %>%
+    fitBounds(~min(lon), ~min(lat), ~max(lon), ~max(lat))
   })
+
+observe({
+  pal <- colorpal()
   
-  output$map <- renderLeaflet({
-    # Use leaflet() here, and only include aspects of the map that
-    # won't need to change dynamically (at least, not unless the
-    # entire map is being torn down and recreated).
-    leaflet(quakes) %>% addTiles() %>%
-      fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
-  })
+  leafletProxy("map", data = filteredData()) %>%
+    clearShapes() %>%
+    addCircles(radius = ~1000^MPI, weight = 6.5, color = "#777777",
+               fillColor = ~pal(MPI), fillOpacity = 0.7, popup = ~paste(MPI),
+               label = labels,
+               labelOptions = labelOptions(
+                 style = list("font-weight" = "normal", padding = "3px 8px"),
+                 textsize = "15px",
+                 direction = "auto")
+    )
+})
+
+observe({
+  proxy <- leafletProxy("map", data = kiva_mpi)
   
-  # Incremental changes to the map (in this case, replacing the
-  # circles when a new color is chosen) should be performed in
-  # an observer. Each independent set of things that can change
-  # should be managed in its own observer.
-  observe({
+  # Remove any existing legend, and only if the legend is
+  # enabled, create a new one.
+  proxy %>% clearControls()
+  if (input$legend) {
     pal <- colorpal()
-    
-    leafletProxy("map", data = filteredData()) %>%
-      clearShapes() %>%
-      addCircles(radius = ~10^mag/10, weight = 1, color = "#777777",
-                 fillColor = ~pal(mag), fillOpacity = 0.7, popup = ~paste(mag)
-      )
-  })
-  
-  # Use a separate observer to recreate the legend as needed.
-  observe({
-    proxy <- leafletProxy("map", data = quakes)
-    
-    # Remove any existing legend, and only if the legend is
-    # enabled, create a new one.
-    proxy %>% clearControls()
-    if (input$legend) {
-      pal <- colorpal()
-      proxy %>% addLegend(position = "bottomright",
-                          pal = pal, values = ~mag
-      )
-    }
-  })
+    proxy %>% addLegend(position = "bottomright",
+                        pal = pal, values = ~MPI
+    )
+  }
+})
 }
 
 shinyApp(ui, server)
--- week_5/app.R
+++ week_5/app.R
@@ -1,60 +1,70 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-library(ggplot2)
+library(leaflet)
+library(RColorBrewer)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("binsBar",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30),
-         sliderInput("intervals",
-                      "Intervals:",
-                      min = 0, max = 6,
-                      value = c(1,2))
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot"),
-         plotOutput("distPlot2")
-      )
-   )
+ui <- bootstrapPage(
+  tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+  leafletOutput("map", width = "100%", height = "100%"),
+  absolutePanel(top = 10, right = 10,
+                sliderInput("range", "Magnitudes", min(quakes$mag), max(quakes$mag),
+                            value = range(quakes$mag), step = 0.1
+                ),
+                selectInput("colors", "Color Scheme",
+                            rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
+                ),
+                checkboxInput("legend", "Show legend", TRUE)
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      binsBar <- seq(min(x), max(x), length.out = input$binsBar + 1)
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = binsBar, col = 'darkgray', border = 'white')
-   })
+server <- function(input, output, session) {
   
-  output$distPlot2 <- renderPlot({
-    ggplot(faithful, aes(x = eruptions, y = waiting)) +
-      geom_point() + xlim(input$intervals)
+  # Reactive expression for the data subsetted to what the user selected
+  filteredData <- reactive({
+    quakes[quakes$mag >= input$range[1] & quakes$mag <= input$range[2],]
   })
-  }
+  
+  # This reactive expression represents the palette function,
+  # which changes as the user makes selections in UI.
+  colorpal <- reactive({
+    colorNumeric(input$colors, quakes$mag)
+  })
+  
+  output$map <- renderLeaflet({
+    # Use leaflet() here, and only include aspects of the map that
+    # won't need to change dynamically (at least, not unless the
+    # entire map is being torn down and recreated).
+    leaflet(quakes) %>% addTiles() %>%
+      fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
+  })
+  
+  # Incremental changes to the map (in this case, replacing the
+  # circles when a new color is chosen) should be performed in
+  # an observer. Each independent set of things that can change
+  # should be managed in its own observer.
+  observe({
+    pal <- colorpal()
+    
+    leafletProxy("map", data = filteredData()) %>%
+      clearShapes() %>%
+      addCircles(radius = ~10^mag/10, weight = 1, color = "#777777",
+                 fillColor = ~pal(mag), fillOpacity = 0.7, popup = ~paste(mag)
+      )
+  })
+  
+  # Use a separate observer to recreate the legend as needed.
+  observe({
+    proxy <- leafletProxy("map", data = quakes)
+    
+    # Remove any existing legend, and only if the legend is
+    # enabled, create a new one.
+    proxy %>% clearControls()
+    if (input$legend) {
+      pal <- colorpal()
+      proxy %>% addLegend(position = "bottomright",
+                          pal = pal, values = ~mag
+      )
+    }
+  })
+}
 
-# Run the application 
-shinyApp(ui = ui, server = server)
-
+shinyApp(ui, server)
